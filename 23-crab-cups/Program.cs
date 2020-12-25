b10cf3e08f32cc94f63fe1748b8fb62bf3ba46fa
Daric Teske <tesked@outlook.com>
2020-12-25 13:56:21 -0600

    Not sure, still not getting the right answer

--- 23-crab-cups/Program.cs
+++ 23-crab-cups/Program.cs
@@ -10,10 +10,11 @@ namespace CrabCups
     {
         static void Main(string[] args)
         {
-            var filePath = @"R:\Code\2020-advent-of-code\CrabCups\sample.txt";
+            var filePath = @"R:\Code\2020-advent-of-code\23-crab-cups\sample.txt";
             var line = File.ReadAllLines(filePath).First();
 
             PartOne(line);
+            PartOneRedo(line);
             PartTwo(line);
         }
 
@@ -21,16 +22,20 @@ namespace CrabCups
         {
             var cups = line.Select(t => int.Parse(t.ToString())).ToList();
             var cupsMax = cups.Max();
-            var list = new LinkedListNode<int> { Value = cups[0] };
+            var lookup = new Dictionary<int, LinkedListNode<int>>();
+            lookup.Add(cups[0], new LinkedListNode<int> { Value = cups[0] });
+            var list = lookup[cups[0]];
             var currentCup = list;
             for (var i = 1; i < cups.Count; i++)
             {
-                currentCup.Next = new LinkedListNode<int> { Value = cups[i] };
+                lookup.Add(cups[i], new LinkedListNode<int> { Value = cups[i] });
+                currentCup.Next = lookup[cups[i]];
                 currentCup = currentCup.Next;
             }
             for (var i = cupsMax + 1; i < 1_000_000; i++)
             {
-                currentCup.Next = new LinkedListNode<int> { Value = i };
+                lookup.Add(i, new LinkedListNode<int> { Value = i });
+                currentCup.Next = lookup[i];
                 currentCup = currentCup.Next;
             }
             currentCup.Next = list;
@@ -38,8 +43,6 @@ namespace CrabCups
             currentCup = currentCup.Next;
             for (int i = 0; i < 10_000_000; i++)
             {
-                Console.Write($"\rProgress: {i}");
-
                 var startingCupToRemove = currentCup.Next;
                 var excludedValues = new List<int> { startingCupToRemove.Value, startingCupToRemove.Next.Value, startingCupToRemove.Next.Next.Value };
                 currentCup.Next = startingCupToRemove.Next.Next.Next;
@@ -62,14 +65,68 @@ namespace CrabCups
                         targetCupValue--;
                     }
                 }
-                var targetNode = FindNode(currentCup, targetCupValue);
+                var targetNode = lookup[targetCupValue];
                 startingCupToRemove.Next.Next.Next = targetNode.Next;
                 targetNode.Next = startingCupToRemove;
                 currentCup = currentCup.Next;
             }
+            var start = lookup[1];
+            long result = (long)start.Next.Value * start.Next.Next.Value;
+            Console.WriteLine($"Solution 2: {start.Next.Value} * {start.Next.Next.Value}");
 
-            var start = FindNode(list, 1);
-            Console.WriteLine($"Solution 2: {start.Next.Value * start.Next.Next.Value}");
+            // 776176127776 is too high
+            Console.WriteLine($"Solution 2: {result}");
+        }
+
+        private static void PartOneRedo(string line)
+        {
+            var cups = line.Select(t => int.Parse(t.ToString())).ToList();
+            var cupsMax = cups.Max();
+            var lookup = new Dictionary<int, LinkedListNode<int>>();
+            lookup.Add(cups[0], new LinkedListNode<int> { Value = cups[0] });
+            var list = lookup[cups[0]];
+            var currentCup = list;
+            for (var i = 1; i < cups.Count; i++)
+            {
+                lookup.Add(cups[i], new LinkedListNode<int> { Value = cups[i] });
+                currentCup.Next = lookup[cups[i]];
+                currentCup = currentCup.Next;
+            }
+            currentCup.Next = list;
+
+            currentCup = currentCup.Next;
+            for (int i = 0; i < 100; i++)
+            {
+                var startingCupToRemove = currentCup.Next;
+                var excludedValues = new List<int> { startingCupToRemove.Value, startingCupToRemove.Next.Value, startingCupToRemove.Next.Next.Value };
+                currentCup.Next = startingCupToRemove.Next.Next.Next;
+
+                var nextFound = false;
+                var targetCupValue = currentCup.Value - 1;
+                while (!nextFound)
+                {
+                    targetCupValue = Euclid.Modulus(targetCupValue, cupsMax);
+                    if (targetCupValue == 0)
+                    {
+                        targetCupValue = cupsMax;
+                    }
+                    if (!excludedValues.Contains(targetCupValue))
+                    {
+                        nextFound = true;
+                    }
+                    else
+                    {
+                        targetCupValue--;
+                    }
+                }
+                var targetNode = lookup[targetCupValue];
+                startingCupToRemove.Next.Next.Next = targetNode.Next;
+                targetNode.Next = startingCupToRemove;
+                currentCup = currentCup.Next;
+            }
+            var start = lookup[1];
+            //Console.WriteLine($"Solution 1.2: {start.Next.Value} * {start.Next.Next.Value}");
+            PrintList(lookup[1]);
         }
 
         private static void PartOne(string line)
@@ -123,17 +180,6 @@ namespace CrabCups
             newList.AddRange(endRange);
             newList.AddRange(startingList.GetRange(0, startingList.Count - newList.Count));
             return newList;
-        }
-
-
-        static LinkedListNode<int> FindNode(LinkedListNode<int> node, int val)
-        {
-            var currentNode = node;
-            while (currentNode.Value != val)
-            {
-                currentNode = currentNode.Next;
-            }
-            return currentNode;
         }
 
         static void PrintList(LinkedListNode<int> node)
